e79e69f30b9ae584ff2c873cf85d77afcef3515b
Gustavo <noronhadesouzag@gmail.com>
2025-11-12 10:37:16 -0300

    primeiro commit

---
+++ passoia/src/App.jsx
@@ -0,0 +1,14 @@
+// Aqui importamos os arquivos
+import Header from "./components/Header"
+
+// Arquivo principal
+// Para escrita do nome do componente funcional usamos sempre a primeira letra maíuscula, PascalCase
+function App() {
+  return (
+    <>
+      <header/>
+    </>
+  )
+}
+export default App
+
---
+++ passoia/src/Header/index.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header"
+
+function App() {
+    return (
+        <header>
+            <img src="" alt="Logo da marca da página" />
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADE</li>
+            </ul>
+        </header>
+    )
+}
+export default App
